e1bc1cbe734d3e3f9a6ae7ffe2f9bcfb1fa48d67
vineetl007 <vineet.loomba76@gmail.com>
2025-07-03 10:40:44 +0530

    Update config.js

--- static/admin/config.js
+++ static/admin/config.js
@@ -1,24 +1,25 @@
+import CMS from "https://unpkg.com/decap-cms@3.1.0/dist/decap-cms.js";
+
 CMS.init({
   config: {
     backend: {
       name: "git-gateway",
       branch: "main",
     },
-    load_config_file: false,
     media_folder: "static/uploads",
     public_folder: "/uploads",
     collections: [
       {
-        name: "calculus",
-        label: "Calculus",
+        name: "chapters",
+        label: "Chapters",
         folder: "content/calculus",
         create: true,
         slug: "{{slug}}",
         fields: [
           { label: "Title", name: "title", widget: "string" },
-          { label: "Body", name: "body", widget: "markdown" }
-        ]
-      }
-    ]
-  }
+          { label: "Body", name: "body", widget: "markdown" },
+        ],
+      },
+    ],
+  },
 });

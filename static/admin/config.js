5bcc5b6a4a1d2d395271279ccad27b9ae9c5d446
vineetl007 <vineet.loomba76@gmail.com>
2025-08-15 18:29:53 +0530

    Update config.js

--- static/admin/config.js
+++ static/admin/config.js
@@ -21,6 +21,7 @@ CMS.init({
     fields: [
       { label: "Title", name: "title", widget: "string" },
       { label: "Weight", name: "weight", widget: "number", default: 1 },
+      { label: "YouTube Video Link", name: "video", widget: "string", required: false },
       { label: "Body", name: "body", widget: "markdown" },
     ],
   },
@@ -32,7 +33,8 @@ CMS.init({
   slug: "{{slug}}",
   fields: [
     { label: "Title", name: "title", widget: "string" },
-   { label: "Weight", name: "weight", widget: "number", default: 1 },
+    { label: "Weight", name: "weight", widget: "number", default: 1 },
+    { label: "YouTube Video Link", name: "video", widget: "string", required: false },
     { label: "Body", name: "body", widget: "markdown" },
     ],
   },

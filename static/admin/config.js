1aff42802558adf1b2f295b342c7def9aa890987
vineetl007 <vineet.loomba76@gmail.com>
2025-08-22 14:15:06 +0530

    Update config.js

--- static/admin/config.js
+++ static/admin/config.js
@@ -22,7 +22,7 @@ CMS.init({
       { label: "Title", name: "title", widget: "string" },
       { label: "Weight", name: "weight", widget: "number", default: 1 },
       { label: "Date", name: "date", widget: "datetime", format: "YYYY-MM-DD", required: false },
-      { label: "Description", name: "description", widget: "string", hint: "SEO meta description (around 150 characters)" }
+      { label: "Description", name: "description", widget: "string", hint: "SEO meta description (around 150 characters)" },
       { label: "Body", name: "body", widget: "markdown" },
     ],
   },
@@ -35,7 +35,7 @@ CMS.init({
   fields: [
     { label: "Title", name: "title", widget: "string" },
     { label: "Weight", name: "weight", widget: "number", default: 1 },
-    { label: "Description", name: "description", widget: "string", hint: "SEO meta description (around 150 characters)" }
+    { label: "Description", name: "description", widget: "string", hint: "SEO meta description (around 150 characters)" },
     { label: "Body", name: "body", widget: "markdown" },
     ],
   },

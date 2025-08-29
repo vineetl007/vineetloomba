3168351e305846a27194d99b98ac52b6b88ad02c
vineetl007 <vineet.loomba76@gmail.com>
2025-08-29 19:22:53 +0530

    Update practice.js

--- static/js/practice.js
+++ static/js/practice.js
@@ -110,7 +110,7 @@ ${tags.length ? tags.map(tag => `
 ${q.video_url ? `
   <div class="mt-6">
     <p class="text-lg font-semibold mb-2">🎥 Video Solution</p>
-<div style="position:relative;padding-bottom:56.25%;height:0;overflow:hidden;margin-top:1.5rem;margin-bottom:1.5rem;">
+ <div style="position:relative;padding-bottom:56.25%;height:0;overflow:hidden;border-radius:12px;">
       <iframe 
         src="${q.video_url}"
         style="position:absolute;top:0;left:0;width:100%;height:100%;"

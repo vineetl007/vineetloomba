d58e11012d39cae37af8da5e2f9636e08c6a6ce9
vineetl007 <vineet.loomba76@gmail.com>
2025-09-07 18:38:28 +0530

    Update mocktest.js

--- static/js/mocktest.js
+++ static/js/mocktest.js
@@ -650,7 +650,7 @@ const chartHtml = `
           </thead>
           <tbody>
             ${Object.entries(window.analysisData.timeSpent).map(([subj, mins]) => `
-              <tr class="border-b border-gray-700">
+              <tr class="border-b border-gray-700 text-center">
                 <td class="px-3 py-1">${subj}</td>
                 <td class="px-3 py-1">${mins}</td>
               </tr>
@@ -722,7 +722,7 @@ if (ctxScore) {
   // --- Table rows ---
   const tbody = document.getElementById("score-table-body");
   tbody.innerHTML = subjectScores.map(s => `
-    <tr class="border-b border-gray-700">
+    <tr class="border-b border-gray-700 text-center">
       <td class="px-3 py-2">${s.subj}</td>
       <td class="px-3 py-2 text-green-400">${s.total}</td>
       <td class="px-3 py-2 text-red-400">${s.negative}</td>

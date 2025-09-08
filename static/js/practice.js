78b4e74dd974b7b3f31cfd94e62023e0336e053f
vineetl007 <vineet.loomba76@gmail.com>
2025-09-08 18:01:08 +0530

    Update practice.js

--- static/js/practice.js
+++ static/js/practice.js
@@ -63,11 +63,7 @@ const questionHtml = rawQ
 // ✅ Then build the HTML normally
 app.innerHTML = `
   <div class="question border rounded-lg p-4 shadow mb-4">
-    <h2 class="font-normal mb-3">Q${index + 1}.</h2>
-    <div class="question-text mb-3">${questionHtml}</div>
-
-
-
+    
   <div class="mb-4 flex flex-wrap gap-2">
 ${tags.length ? tags.map(tag => `
   <a href="/tags/${encodeURIComponent(tag.toLowerCase())}/"
@@ -91,6 +87,10 @@ ${tags.length ? tags.map(tag => `
     </a>
   ` : ''}
 </div>
+
+    <h2 class="font-normal mb-3">Q${index + 1}.</h2>
+    <div class="question-text mb-3">${questionHtml}</div>
+
         ${
           isInteger
             ? `

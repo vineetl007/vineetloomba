945c8c8774e9142c10e2fed421a8f8bd042f9986
vineetl007 <vineet.loomba76@gmail.com>
2025-09-19 16:40:10 +0530

    Update mocktest.js

--- static/js/mocktest.js
+++ static/js/mocktest.js
@@ -1173,13 +1173,13 @@ app.querySelectorAll(".toggle-solution").forEach(btn => {
 
 
 
-
+/*
   // Safe MathJax typeset
   if (window.MathJax) {
     if (typeof MathJax.typesetPromise === "function") MathJax.typesetPromise().catch(() => {});
     else if (MathJax.Hub && typeof MathJax.Hub.Queue === "function") MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
   }
-}
+} */
 
 
 function submitTest() {
